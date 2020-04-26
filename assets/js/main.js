d0046332beea64817216a86f1b3551521802d907
falcerdev <falcerdeveloper@gmail.com>
2020-04-26 15:25:33 +0700

    starter

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,24 +1,5 @@
 const main = function () {
-	const hero = document.querySelector(".hero");
-	const imgs = document.querySelectorAll(".parallax img");
-
-	window.addEventListener("scroll", function () {
-		let windowY = window.scrollY;
-
-		hero.style.transform = `translate(-50%, ${windowY / 2}%)`;
-
-		imgs[0].style.transform = `translate(0px, ${windowY / 20}%)`;
-
-		imgs[1].style.transform = `translate(0px, ${windowY / 30}%)`;
-
-		imgs[2].style.transform = `translate(0px, ${windowY / 40}%)`;
-
-		imgs[3].style.transform = `translate(0px, ${windowY / 50}%)`;
-
-		imgs[4].style.transform = `translate(0px, ${windowY / 60}%)`;
-
-		imgs[5].style.transform = `translate(0px, ${windowY / 70}%)`;
-	});
+	console.log("Hello World");
 };
 
 // Execute main function
